491ff39f8709284f21826aa0685d3de4531018a6
Eduardo Chaves <al70611@utad.eu>
2022-03-12 12:11:27 +0000

    Upload Exercicios part 1

---
+++ Exercicios/pl02c1/UWApp1/UWApp1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWApp1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = textBox2.Text;
+            textBox2.Text = "";
+        }
+    }
+}
